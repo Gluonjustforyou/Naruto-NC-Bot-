ce48f16c6479b8fb2e5ed880bf56572e92a253cf
Awa <95723716+Gluonjustforyou@users.noreply.github.com>
2023-05-21 13:06:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,35 +2,33 @@ import discord
 from discord.ext import commands
 import socket
 
-intents = discord.Intents.default()
+intents = discord.Intents.all()
 intents.typing = False
 intents.presences = False
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
 @bot.event
 async def on_ready():
-  print(f'Eingeloggt als {bot.user.name} ({bot.user.id})')
-
+    print(f'Eingeloggt als {bot.user.name} ({bot.user.id})')
 
 @bot.command()
 async def checkport(ctx):
-  hostname = 'de-fra-game1.ips-hosting.com' #this shi ist just for me u need some kind of adrress
-  port = 2200 # just the prot 
-  try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.settimeout(1)
-    result = sock.connect_ex((hostname, port))
-    sock.close()
+    hostname = 'de-fra-game1.ips-hosting.com' #this shi ist just for me u need some kind of adrress
+    port = 2200 #just the prot 
 
-    if result == 0:
-      await ctx.send(f"Port {port} auf {hostname} ist offen.")
-    else:
-      await ctx.send(f"Port {port} auf {hostname} ist geschlossen.")
-  except socket.error as e:
-    await ctx.send(f"Fehler beim Prüfen des Ports: {e}")
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.settimeout(1)
+        result = sock.connect_ex((hostname, port))
+        sock.close()
 
+        if result == 0:
+            await ctx.send(f"Port {port} auf {hostname} ist offen.")
+        else:
+            await ctx.send(f"Port {port} auf {hostname} ist geschlossen.")
+    except socket.error as e:
+        await ctx.send(f"Fehler beim Prüfen des Ports: {e}")
 
 bot.run('Token')
 #no comms cuz it's Logic
